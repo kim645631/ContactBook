70b5c9752efe9e78505f0d35c232decf41159a15
HSU,WEI-YU <41343121@nfu.edu.tw>
2025-11-17 15:33:17 +0800

    D

--- mywidget.cpp
+++ mywidget.cpp
@@ -3,6 +3,9 @@
 
 #include<QFile>
 #include<QDebug>
+#include<QFileDialog>
+#include<QTextStream>
+#include<QApplication>
 
 QString mFilename = "C:/Users/user/Desktop/EX/myfile.txt";
 
@@ -17,6 +20,18 @@ void Write(QString Filename,QString str)
     out<<str;
     mFile.flush();
     mFile.close();
+}
+QString Read(QString Filename)
+{
+    QFile mFile(Filename);
+    if(!mFile.open(QFile::ReadOnly | QFile::Text)){
+        qDebug()<<"could not open file for read";
+        return "";
+    }
+    QTextStream in(&mFile);
+    QString text = in.readAll();
+    mFile.close();
+    return text;
 }
 
 MyWidget::MyWidget(QWidget *parent)
@@ -61,6 +76,7 @@ void MyWidget::on_pushButton_2_clicked()
 void MyWidget::on_pushButton_clicked()
 {
     QString saveFile="";
+    mFilename=QFileDialog::getSaveFileName(this,"匯出存檔",".");
     for(int i=0;i<ui->tableWidget->rowCount();i++){
         for(int j=0;j<ui->tableWidget->columnCount();j++){
             saveFile+=ui->tableWidget->item(i,j)->text()+",";
@@ -68,5 +84,71 @@ void MyWidget::on_pushButton_clicked()
         saveFile+="\n";
     }
     Write(mFilename,saveFile);
+}
+void MyWidget::on_pushButton_3_clicked()
+{
+    mFilename=QFileDialog::getOpenFileName(this,"匯入存檔",".");
+    QString fileContent = Read(mFilename);
+    if(fileContent.isEmpty()){
+        qDebug()<<"File is empty or could not be read";
+        return;
+    }
+
+    // Clear existing table data
+    ui->tableWidget->setRowCount(0);
+
+    // Split by lines
+    QStringList lines = fileContent.split("\n", Qt::SkipEmptyParts);
+
+    for(int i=0; i<lines.size(); i++){
+        QString line = lines[i];
+        QStringList fields = line.split(",");
+
+        if(fields.size() >= 4){
+            ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+
+            for(int j=0; j<4 && j<fields.size(); j++){
+                QTableWidgetItem *item = new QTableWidgetItem(fields[j]);
+                ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, j, item);
+            }
+        }
+    }
+}
+
+
+void MyWidget::on_pushButton_4_clicked()
+{
+    // Save data first
+    on_pushButton_clicked();
+    // Then close the application
+    close();
+    QString text = Read(mFilename);
+    if(text.isEmpty()){
+        qDebug()<<"No data to import or file is empty";
+        return;
+    }
+
+    // Clear existing table data
+    ui->tableWidget->setRowCount(0);
+
+    // Split by lines
+    QStringList lines = text.split("\n", Qt::SkipEmptyParts);
+
+    for(const QString &line : lines){
+        // Split by comma
+        QStringList fields = line.split(",");
+
+        if(fields.size() == 4){
+            // Add a new row
+            int row = ui->tableWidget->rowCount();
+            ui->tableWidget->insertRow(row);
+
+            // Set the data for each column
+            for(int i = 0; i < 4; i++){
+                QTableWidgetItem *item = new QTableWidgetItem(fields[i]);
+                ui->tableWidget->setItem(row, i, item);
+            }
+        }
+    }
 }
 
